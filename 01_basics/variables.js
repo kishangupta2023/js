c2ddfaff4f2ced98454e4360d24b6aee08c88c28
kishan gupta <kishan.2023ug1075@iiitranchi.ac.in>
2025-05-14 04:03:53 +0000

    in baiscs lecture

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accoutEmail = "kishan123n1@gmail.com"
+var accountPassword = "12345"
+accountCity ="Jaipur"
+
+// accountId = 2  //not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword ="223232"
+accountCity="bengaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
