ed808bc522ceed2075a7a80d9f82ed645598f7bd
kishan gupta <kishan.2023ug1075@iiitranchi.ac.in>
2025-05-14 04:42:19 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+// alert("3+3") //we are using nodejs , not browser
+console.log(3+3) // code readibility should be high
+console.log("hitesh")
+// tc39.ts and mdn watch these documentation when required 
+
+let name = "kishan"
+let age = 18
+let isLoggedIn = false
+let state;  //undefined
+
+
+// primitive datatype -->
+// number => 2 to power 53
+//bigint 
+// string  = ""
+//boolean => true or false
+// null - it is stand alone value
+// undefined => 
+// symbol => unique
+// null -> it is an object 
+console.log(typeof null);
+console.log(typeof undefined);
