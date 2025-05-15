d39a926971f754b1fec5516ed3fb43db4cdd497d
kishan gupta <kishan.2023ug1075@iiitranchi.ac.in>
2025-05-15 01:17:42 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+console.log(undefined ==0);
+console.log(undefined >0);
+console.log(undefined <0);
+// we shoukd avoid undefined and null comparison 
+
+
+// === also checks the datatype 
+
+console.log("2"===2);
