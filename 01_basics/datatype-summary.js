b6176ee41bdbf4e5b4b28e597bcf069a9ffbf54c
kishan gupta <kishan.2023ug1075@iiitranchi.ac.in>
2025-05-15 01:38:47 +0000

    datatype summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,33 @@
+//  # primitive
+// -> 7 types : Sting , Number , Boolean ,null,Undefined,Symbol,BigInt
+
+const score = 100
+const scorevalue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id =Symbol('123')
+const anotherId =Symbol('123')
+
+// console.log(id == anotherId);
+
+const bigNumber =3475445454545444n
+
+
+
+// Reference (Non primitive)
+// Array ,Objects,Functions
+
+const heros = ["shaktiman","naagraj","doga"];
+
+let myobj={
+    name:"kishan",
+    age: 22,
+}
+
+const myFunction =function(){
+     console.log("HEllo world");
+}
+
+console.log(typeof anotherId);
+// https://262.ecma-international.org/5.1/#sec-11.4.3
